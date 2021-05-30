97645acecf4da2269cf616ccbdee8647ffee115a
zionhjs <hjszion@gmail.com>
2021-05-29 19:22:17 -0700

    full

--- starwars/src/components/Planets.js
+++ starwars/src/components/Planets.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import { useQuery } from 'react-query';
+import React, { useState } from 'react';
+import { useQuery, usePaginatedQuery } from 'react-query';
 import Planet from './Planet';
 
-const fetchPlanets = async () => {
-  const res = await fetch('http://swapi.dev/api/planets/');
+const fetchPlanets = async (key, page) => {
+  const res = await fetch(`http://swapi.dev/api/planets/?page=${page}`);
   return res.json();
 }
 
 const Planets = () => {
-  const { data, status } = useQuery('planets', fetchPlanets);
-  console.log(data);
+  const [ page, setPage ] = useState(1);
+  // const { data, status } = useQuery(['planets', 'hello, ninjas', page], fetchPlanets);
+  const { resolvedData, latestData, status } = usePaginatedQuery(['planets', page], fetchPlanets);
 
   return (
     <div>
@@ -24,9 +25,20 @@ const Planets = () => {
       )}
 
       {status === 'success' && (
-        <div>
-          { data.results.map(planet => <Planet key={planet.name} planet={planet} /> ) }
-        </div>
+          <>
+            <button
+                onClick={() => setPage(page => Math.max(page-1, 1))}
+                disabled={page === 1}
+            >Previous Page</button>
+            <button>{ page }</button>
+            <button
+                onClick={() => setPage(page => Math.max(page+1, 1))}
+                disabled={!latestData || !latestData.next}
+            >Next Page</button>
+            <div>
+              { resolvedData.results.map(planet => <Planet key={planet.name} planet={planet} /> ) }
+            </div>
+         </>
       )} 
     </div>
   );
